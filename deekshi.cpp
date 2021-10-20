be855cd9597e684626f281bd664e8b978303d833
deekshitha <92291129+manideekshitha@users.noreply.github.com>
2021-10-20 12:29:42 +0530

    Add files via upload

--- deekshi.cpp
+++ deekshi.cpp
@@ -1,5 +1,18 @@
-#include<stdio.h>
- int main()
+//c programme to print age,cgpa and age.
+ #include<stdio.h>{ 
+ void main()
+ //variable declaration
  {
- 	printf("HELLO WORLD.my name is DEEKSHITHA");
- }
+ int age;
+ float cgpa;
+ char initial;
+ //instalization
+ age=20;
+ cgpa=8.5;
+ initial=e;
+ //output
+ printf("age of the student is:%d\n",age);
+ printf("cgpa of the student is:%f\n",cgpa);
+ printf("student surname starts with:%c",initial);
+}
+
